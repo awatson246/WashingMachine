59abc63cf58811cf7d4b4adde660689b77e37e81
watsonation <35076820+awatson246@users.noreply.github.com>
2022-02-17 17:50:08 -0500

    Create delay_ms.h

---
+++ delay_ms.h
@@ -0,0 +1,15 @@
+/*
+ * delay_ms.h
+ *
+ * Created: 2/17/2022
+ *  Author: Adriana Watson
+ */ 
+
+
+#ifndef DELAY_MS_H_
+#define DELAY_MS_H_
+
+void Delay_1mS(void);
+void ms_Delay(uint16_t time);
+
+#endif /* DELAY_MS_H_ */
